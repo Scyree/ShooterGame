2abc5bf957eb49ae833935ae575ad9cb7d303083
Radu-Cristian Vulpescu <radu.vulpescu16@gmail.com>
2017-11-26 18:12:01 +0200

    Added project

---
+++ WAgI/WAgI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WAgI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
