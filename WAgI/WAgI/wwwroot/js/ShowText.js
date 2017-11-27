8394a70b7f98716689105b7fc00c09456e5a9a12
Razvan Cimpoesu <razavnvs@gmail.com>
2017-11-27 22:32:38 +0200

    Added About page(including images, javascrips and text)

---
+++ WAgI/WAgI/wwwroot/js/ShowText.js
@@ -0,0 +1,8 @@
+﻿function show(target){
+    document.getElementById(target).style.display = 'block';
+    document.getElementById("clickMeId").style.display = 'none';
+}
+function hide(target){
+    document.getElementById(target).style.display = 'none';
+    document.getElementById("clickMeId").style.display = 'block';
+}
