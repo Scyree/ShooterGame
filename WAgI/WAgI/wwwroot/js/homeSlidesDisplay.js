92d72a02535660a0a560679c922bcd79bab6c0e8
Adrian Dorneanu <adrian.dorneanu96@gmail.com>
2017-11-27 21:11:29 +0200

    Added responsivity to navBar and Index page (portrait + landscape)

--- WAgI/WAgI/wwwroot/js/homeSlidesDisplay.js
+++ WAgI/WAgI/wwwroot/js/homeSlidesDisplay.js
@@ -2,15 +2,15 @@
 var slides = document.getElementsByClassName("slides");
 var i;
 
-//for (i = 0; i < slides.length; i++) {
- //   slides[i].style.display = "none";
-//}
-
 function plusSlides(value) {
     showSlides(slideIndex += value);
 }
 
 function showSlides(n) {
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
     
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
